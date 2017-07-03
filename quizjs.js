adfa6441985c152df66bc6d370b30fe53f66f1f8
Roxsy Velasquez <roxsy.lj@gmail.com>
2017-07-03 12:57:40 -0500

    quiz1

---
+++ quizjs.js
@@ -0,0 +1,43 @@
+
+//EJERCICIO-1
+/*var num1 = prompt();
+
+var num2 = prompt();
+
+var num3 = prompt();
+
+if (num1>num2 && num1>num3){
+document.write(" Mayor " + num1 +  " Menor " + num3);
+}
+
+
+if (num2>num1 && num2>num3){
+document.write(" Mayor " + num2 +  " Menor " + num3);
+}
+
+if (num3>num1 && num3>num2){
+document.write(" Mayor " + num3 +  " Menor " + num2);
+}
+*/
+
+//EJERCICIO-2
+/*
+var numeros = [25, 11, 5, 3, 8, 9];
+ 
+var minimo=Math.min.apply(null, numeros);
+var maximo=Math.max.apply(null, numeros);
+document.write(" Minimo "+minimo)
+document.write(" Maximo "+maximo)
+*/
+
+//EJERCICIO-3
+var num = prompt[];
+function calcula(num)
+{
+if (var i = in i) {
+   document.write("Indices  pares");
+} else {
+   document.write("Indices impars");
+}
+return (calcula)
+}
