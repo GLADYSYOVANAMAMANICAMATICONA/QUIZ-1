d3bd0694e00870f4c15e29ee390f3ab2178689f7
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-07-03 15:45:53 -0500

    Update quizjs.js

--- quizjs.js
+++ quizjs.js
@@ -42,5 +42,25 @@ else { impar +=i;
 }
 */
 //EJERCICIO 3
+/*
 var numeros = [1, 2, 6, 5];
-document.write (numeros.reverse());
+document.write (numeros.reverse());*/
+//EJERCICIO 4
+var n = 5;
+
+function espacios (n)
+{
+  var s = "";
+  for (var i = 0; i < n; i++)
+   s += " ";
+  return s;
+}
+
+for (var i = 0; i < n; i++) {
+  var sp = espacios (n - (i+1));
+  var content = "";
+  for (j = 0; j < i+i; j++)
+    content +=  j;
+  console.log(sp + content);
+}
+
